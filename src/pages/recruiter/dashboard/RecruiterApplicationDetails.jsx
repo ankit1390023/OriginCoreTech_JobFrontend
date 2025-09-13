5205c2f948f315d82f213084dbcd52397d0ee8e4
dcryptau <dcrypt.au@gmail.com>
2025-09-13 16:18:16 +0530

    stage before file renaming

--- src/pages/recruiter/dashboard/RecruiterApplicationDetails.jsx
+++ src/pages/recruiter/dashboard/RecruiterApplicationDetails.jsx
@@ -200,7 +200,7 @@ const ApplicationDetail = () => {
     <MainLayout>
       <div className="flex items-start justify-center min-h-screen px-2 bg-gray-100 lg:px-4 ">
         {/* <div className="hidden lg:block w-[250px]"></div> */}
-        <aside className="hidden lg:block  max-w-full sticky top-4 h-fit mt-2 py-2 ">
+        <aside className="sticky hidden max-w-full py-2 mt-2 lg:block top-4 h-fit ">
           {/* hidden lg:block w-[350px] xl:w-[450px] 2xl:w-[550px] h-screen sticky top-0 pt-6  overflow-y-auto" */}
           <RecruiterApplicationData job_id={job_id} />
         </aside>
@@ -386,7 +386,7 @@ const ApplicationDetail = () => {
                     {transformedExperiences.map((exp, index) => (
                       <div
                         key={index}
-                        className="pl-4 border-l-4 border-gray-200"
+                        className=""
                       >
                         {" "}
                         {/* Optional: left accent bar like resume */}
